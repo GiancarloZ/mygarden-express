84cd62c04177e21ea74540a0099659dfe0554447
giancarloz <gzabaleta48@gmail.com>
2021-03-15 17:11:29 -0400

    change config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    database: "my_garden_express_api_development",
+    dialect: "postgres",
+    password: "nachoz"
+  },
+  "test": {
+    "database": "my_garden_express_api_test",
+    "dialect": "postgres"
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+}
